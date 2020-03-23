783ffef9e368e8159ddf4e9f43ebdaeb17a6d24b
achus <arshaqhameed@gmail.com>
2020-03-23 16:24:53 +0530

    almost completed

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    
+    text = models.CharField(max_length=45)
+    complete = models.BooleanField(default=False)
+    def __str__(self):
+        return self.text
